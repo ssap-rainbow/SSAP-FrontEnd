6b2587aa8ae3b7170c23f8136fa63951e18dc0d6
Seongsuk Hong <ssukh77@naver.com>
2023-11-06 20:50:11 +0900

    chore(환경 변수 수정)

--- frontend/src/apis/OAuth.ts
+++ frontend/src/apis/OAuth.ts
@@ -1,7 +1,5 @@
-const CLIENT_ID = process.env.REACT_APP_REST_API_KEY;
-// const CLIENT_ID = process.env.KAKAO_CLIENT_ID;
+const CLIENT_ID = REACT_APP_REST_API_KEY;
+const REDIRECT_URI = REACT_APP_REDIRECT_URL;
 
-const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
-// const REDIRECT_URI = process.env.KAKAO_REDIRECT_URL;
-
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=2973b10a274c7944e2e65ead4ef8370e&redirect_uri=https://k0afbd0de17bfa.user-app.krampoline.com/api/oauth/kakao/callback&response_type=code`;
+// export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=2973b10a274c7944e2e65ead4ef8370e&redirect_uri=https://k0afbd0de17bfa.user-app.krampoline.com/api/oauth/kakao/callback&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
