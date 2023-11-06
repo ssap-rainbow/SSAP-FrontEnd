629badc38d9e8d563c71684fcbb2f23658bf328c
장정안 <wjddks96@naver.com>
2023-11-06 16:55:24 +0900

    feat: 환경변수 이름 변경

--- src/apis/OAuth.ts
+++ src/apis/OAuth.ts
@@ -1,5 +1,7 @@
-const CLIENT_ID = process.env.REACT_APP_REST_API_KEY;
+// const CLIENT_ID = process.env.REACT_APP_REST_API_KEY;
+const CLIENT_ID = process.env.KAKAO_CLIENT_ID;
 
-const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
+// const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URL;
+const REDIRECT_URI = process.env.KAKAO_REDIRECT_URL;
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
