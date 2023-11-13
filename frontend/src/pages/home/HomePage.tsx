c336cd987f332197d0d5631ddcc3ddbc9f55a456
장정안 <wjddks96@naver.com>
2023-11-13 18:47:13 +0900

    feat: 라우터 수정

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { LogoutHandler } from "../../apis/Logout";
+import LoginPage from "../Login/LoginPage";
 
 const HomePage = () => {
   const logout = LogoutHandler();
   return (
     <div>
       <button onClick={logout}>로그아웃</button>
+      <LoginPage />
     </div>
   );
 };
