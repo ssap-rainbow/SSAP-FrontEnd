9f3eead6b1f84e3849d7f1003d2e7e67e3d47ff7
장정안 <wjddks96@naver.com>
2023-11-13 19:24:05 +0900

    feat: 엑세스토큰 로컬스토리지 저장 로직 추가

--- frontend/src/pages/Login/LoginHandler.tsx
+++ frontend/src/pages/Login/LoginHandler.tsx
@@ -1,45 +1,39 @@
 import axios from "axios";
 import React, { useEffect } from "react";
-
 import { useNavigate } from "react-router-dom";
 
 export const LoginHandeler = () => {
-  //인가코드 백으로 전달하는 곳
   const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code");
-  console.log("code1", code);
+
   useEffect(() => {
     const kakaoLogin = async () => {
-      console.log("code2", code);
-
-      await axios({
-        method: "GET",
-        url: `https://ke08fd11a16dba.user-app.krampoline.com/api/oauth/kakao/callback/?code=${code}`,
-
-        // url: `${process.env.REACT_APP_REDIRECT_URL}/?code=${code}`,
-        headers: {
-          "Content-Type": "application/json;charset=utf-8", //json형태로 데이터를 보내겠다는뜻
-          "Access-Control-Allow-Origin": "*", //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
-        },
-      })
-        .then((res) => {
-          //백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
-          console.log(res);
-          //계속 쓸 정보들( ex: 이름) 등은 localStorage에 저장해두자
-          localStorage.setItem("name", res.data.account.kakaoName);
-          console.log("code3", code);
-
-          //로그인이 성공하면 이동할 페이지
-          navigate("/");
-          console.log("code4", code);
-        })
-        .catch((error) => {
-          console.error("로그인 요청 실패:", error);
+      try {
+        const res = await axios({
+          method: "GET",
+          url: `https://ke08fd11a16dba.user-app.krampoline.com/api/oauth/kakao/callback/?code=${code}`,
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
+          },
         });
+
+        // 백엔드 응답 처리
+        if (res.data.loginSuccess) {
+          localStorage.setItem("name", res.data.account.userName);
+          localStorage.setItem("accessToken", res.data.accessToken); // 액세스 토큰 저장
+          navigate("/"); // 메인 페이지로 이동
+        } else {
+          // 로그인 실패 처리
+          console.error("로그인 실패");
+        }
+      } catch (error) {
+        console.error("로그인 요청 실패:", error);
+      }
     };
+
     kakaoLogin();
-    console.log("code5", code);
   }, [code]);
+
   return (
     <div>
       <p>로그인 중입니다.</p>
