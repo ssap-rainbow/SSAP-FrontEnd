76ea97280cefca60b7d739b61057e935cadc1c83
장정안 <wjddks96@naver.com>
2023-11-13 18:58:42 +0900

    catch문 추가

--- frontend/src/pages/Login/LoginHandler.tsx
+++ frontend/src/pages/Login/LoginHandler.tsx
@@ -19,14 +19,18 @@ export const LoginHandeler = () => {
           "Content-Type": "application/json;charset=utf-8", //json형태로 데이터를 보내겠다는뜻
           "Access-Control-Allow-Origin": "*", //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
         },
-      }).then((res) => {
-        //백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
-        console.log(res);
-        //계속 쓸 정보들( ex: 이름) 등은 localStorage에 저장해두자
-        localStorage.setItem("name", res.data.account.kakaoName);
-        //로그인이 성공하면 이동할 페이지
-        navigate("/");
-      });
+      })
+        .then((res) => {
+          //백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
+          console.log(res);
+          //계속 쓸 정보들( ex: 이름) 등은 localStorage에 저장해두자
+          localStorage.setItem("name", res.data.account.kakaoName);
+          //로그인이 성공하면 이동할 페이지
+          navigate("/");
+        })
+        .catch((error) => {
+          console.error("로그인 요청 실패:", error);
+        });
     };
     kakaoLogin();
   }, []);
