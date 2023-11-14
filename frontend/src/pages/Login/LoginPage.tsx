9a918ef9cfc2914a2635b533f4417d25ac7ebc47
장정안 <wjddks96@naver.com>
2023-11-14 15:35:10 +0900

    fix: 빌드 실패 수정

--- frontend/src/pages/Login/LoginPage.tsx
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -1,3 +1,13 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>로그인 페이지 입니다! 원래 여기엔 카카오 로그인 버튼이 있어용</div>
+  );
+};
+
+export default LoginPage;
+
 // import React from "react";
 // import { KAKAO_AUTH_URL } from "../../apis/OAuth";
 // import kakaoImage from "../../assets/Kakao.png";
