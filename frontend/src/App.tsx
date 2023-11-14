2ca0a9ba32daf88dd12e8767eb94f422564833e3
장정안 <wjddks96@naver.com>
2023-11-14 11:53:00 +0900

    feat: hashRouter로 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,26 @@
 import React from "react";
 import LoginPage from "./pages/Login/LoginPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import { LoginHandler } from "./pages/Login/LoginHandler";
 import Test from "./pages/Test";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login/oauth2/code/kakao" element={<LoginHandler />} />
-          <Route path="/test" element={<Test />} />
-          {/* <Route path="/" element={<HomePage />} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login/oauth2/code/kakao" element={<LoginHandler />} />
+        <Route path="/test" element={<Test />} />
+        {/* <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route
             // path="/login/oauth2/code/kakao"
             path="/api/oauth/kakao/callback"
             element={<LoginHandler />} //redirect_url에 맞춰 꾸밀 컴포넌트
           /> */}
-        </Routes>
-      </BrowserRouter>
-    </div>
+      </Routes>
+    </HashRouter>
   );
 }
 
