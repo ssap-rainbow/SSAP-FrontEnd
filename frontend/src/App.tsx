14c128a403faf34b3dfc79d4c872a544ab31f1ea
장정안 <wjddks96@naver.com>
2023-11-14 13:24:58 +0900

    feat: createBrowserRouter로 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,59 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import { LoginHandler } from "./pages/Login/LoginHandler";
 import Test from "./pages/Test";
 
+// BrowserRouter 생성
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/login/oauth2/code/kakao",
+    element: <LoginHandler />,
+  },
+  {
+    path: "/test",
+    element: <Test />,
+  },
+  {
+    path: "*",
+    element: <Test />,
+  },
+]);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login/oauth2/code/kakao" element={<LoginHandler />} />
-        <Route path="/test" element={<Test />} />
-        <Route path="*" element={<Test />} />
-        {/* <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            // path="/login/oauth2/code/kakao"
-            path="/api/oauth/kakao/callback"
-            element={<LoginHandler />} //redirect_url에 맞춰 꾸밀 컴포넌트
-          /> */}
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+
+// import React from "react";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import HomePage from "./pages/home/HomePage";
+// import { LoginHandler } from "./pages/Login/LoginHandler";
+// import Test from "./pages/Test";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/login/oauth2/code/kakao" element={<LoginHandler />} />
+//         <Route path="/test" element={<Test />} />
+//         <Route path="*" element={<Test />} />
+//         {/* <Route path="/" element={<HomePage />} />
+//           <Route path="/login" element={<LoginPage />} />
+//           <Route
+//             // path="/login/oauth2/code/kakao"
+//             path="/api/oauth/kakao/callback"
+//             element={<LoginHandler />} //redirect_url에 맞춰 꾸밀 컴포넌트
+//           /> */}
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
