738a925b9f35218a34e430927fc4b13972ac4378
장정안 <wjddks96@naver.com>
2023-11-14 16:45:42 +0900

    제발 ㅠㅠㅠ

--- frontend/src/apis/OAuth.ts
+++ frontend/src/apis/OAuth.ts
@@ -1,4 +1,4 @@
-const CLIENT_ID = process.env.REACT_APP_JAVASCRIPT_API_KEY;
+const CLIENT_ID = process.env.REACT_APP_REST_API_KEY;
 const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
