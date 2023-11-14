71118a423c9b90bed9b4b7186a5054837c1478d9
장정안 <wjddks96@naver.com>
2023-11-14 12:17:37 +0900

    feat: basename 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import LoginPage from "./pages/Login/LoginPage";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import { LoginHandler } from "./pages/Login/LoginHandler";
 import Test from "./pages/Test";
 
 function App() {
   return (
-    <BrowserRouter basename="https://ke08fd11a16dba.user-app.krampoline.com/">
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login/oauth2/code/kakao" element={<LoginHandler />} />
