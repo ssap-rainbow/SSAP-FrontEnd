07ab6c0d029e635931be30242efc8e0657b6c9e8
장정안 <wjddks96@naver.com>
2023-11-14 11:34:42 +0900

    feat: 테스트 페이지 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import LoginPage from "./pages/Login/LoginPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import { LoginHandler } from "./pages/Login/LoginHandler";
+import Test from "./pages/Test";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login/oauth2/code/kakao" element={<LoginHandler />} />
+          <Route path="/test" element={<Test />} />
           {/* <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route
