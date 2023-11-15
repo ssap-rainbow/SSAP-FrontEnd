26096671365c53d977a2b04fd3774660155bd9eb
Seongsuk Hong <ssukh77@naver.com>
2023-11-16 04:59:06 +0900

    fix(REDIRECT_URI 수정)

--- frontend/src/apis/OAuth.ts
+++ frontend/src/apis/OAuth.ts
@@ -1,5 +1,5 @@
 const CLIENT_ID = process.env.REACT_APP_JAVASCRIPT_API_KEY;
-const REDIRECT_URI: string = process.env.REACT_APP_JAVASCRIPT_API_KEY || "";
+const REDIRECT_URI: string = process.env.REACT_APP_REDIRECT_URI || "";
 
 const encodedRedirectUri = encodeURIComponent(REDIRECT_URI);
 
