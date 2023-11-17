3274dba775a45ecd1e7e775217946be819c71d80
장정안 <wjddks96@naver.com>
2023-11-17 16:17:10 +0900

    feat: logout 세션으로 변경

--- frontend/src/apis/Logout.js
+++ frontend/src/apis/Logout.js
@@ -5,8 +5,8 @@ export const LogoutHandler = () => {
   const navigate = useNavigate();
 
   const logout = async () => {
-    const accessToken = localStorage.getItem("accessToken"); // 로컬 스토리지에서 액세스 토큰 가져오기
-    const provider = localStorage.getItem("provider");
+    const accessToken = sessionStorage.getItem("accessToken"); // 로컬 스토리지에서 액세스 토큰 가져오기
+    const provider = sessionStorage.getItem("provider");
     if (!accessToken) {
       console.error("액세스 토큰이 없습니다.");
       return;
@@ -14,7 +14,8 @@ export const LogoutHandler = () => {
 
     try {
       const response = await axios.post(
-        `/api/oauth/${provider}/logout`,
+        `/api/oauth/kakao/logout`,
+        // `/api/oauth/${provider}/logout`,
         {},
         {
           headers: {
@@ -25,9 +26,9 @@ export const LogoutHandler = () => {
 
       if (response.status === 200) {
         console.log("로그아웃 성공");
-        localStorage.removeItem("accessToken"); // 토큰 삭제
-        localStorage.removeItem("name"); // 다른 로컬 스토리지 데이터도 삭제
-        localStorage.removeItem("provider");
+        sessionStorage.removeItem("accessToken"); // 토큰 삭제
+        sessionStorage.removeItem("name"); // 다른 로컬 스토리지 데이터도 삭제
+        sessionStorage.removeItem("provider");
         navigate("/login"); // 로그인 페이지로 리디렉션
       }
     } catch (error) {
