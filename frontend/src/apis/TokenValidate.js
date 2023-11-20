686a62440970cbbeb782b5d392a73abb4ec71688
장정안 <wjddks96@naver.com>
2023-11-21 03:10:32 +0900

    fix: refresh token이 undefined로 출력되는 현상 수정

--- frontend/src/apis/TokenValidate.js
+++ frontend/src/apis/TokenValidate.js
@@ -4,51 +4,56 @@ import { useCookies } from "react-cookie";
 
 function TokenValidate() {
   const [cookies, setCookie] = useCookies(["refreshToken"]);
+
   useEffect(() => {
     console.log("TokenValidate 컴포넌트 실행됨");
+
     const validateToken = async () => {
       const accessToken = sessionStorage.getItem("accessToken");
-      const refreshToken = cookies.refreshToken; // 쿠키에서 리프레시 토큰 가져오기
+      const refreshToken = cookies.refreshToken;
 
       console.log("accessToken :", accessToken);
       console.log("refreshToken :", refreshToken);
 
-      try {
-        const res = await axios({
-          method: "GET",
-          url: `/api/oauth/validate-token?accessToken=${accessToken}`,
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-        console.log("res", res);
-      } catch (error) {
-        if (error.response && error.response.status === 401) {
-          // 401 오류 발생 시, 리프레시 토큰을 사용해서 새로운 엑세스 토큰을 갱신
-          try {
-            const refreshTokenResponse = await axios({
-              method: "GET",
-              url: `/api/oauth/refresh-token?refreshToken=${refreshToken}`,
-            });
-            const newAccessToken = refreshTokenResponse.data;
-            console.log("갱신된 accessToken:", newAccessToken);
+      // refreshToken이 비동기로 설정되었을 때만 실행
+      if (refreshToken) {
+        console.log(
+          "refreshToken이 비동기로 설정되었을 때 refreshToken :",
+          refreshToken
+        );
 
-            // 갱신된 엑세스 토큰을 세션 스토리지에 저장
-            sessionStorage.setItem("accessToken", newAccessToken);
+        try {
+          const res = await axios({
+            method: "GET",
+            url: `/api/oauth/validate-token?accessToken=${accessToken}`,
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+          console.log("res", res);
+        } catch (error) {
+          if (error.response && error.response.status === 401) {
+            try {
+              const refreshTokenResponse = await axios({
+                method: "GET",
+                url: `/api/oauth/refresh-token?refreshToken=${refreshToken}`,
+              });
+              const newAccessToken = refreshTokenResponse.data;
+              console.log("갱신된 accessToken:", newAccessToken);
 
-            // 갱신된 엑세스 토큰으로 다시 요청을 보내야 하나? TODO
-            // 그렇다면 여기에 로직 작성
-          } catch (refreshError) {
-            console.log("엑세스 토큰 갱신 실패", refreshError);
+              sessionStorage.setItem("accessToken", newAccessToken);
+            } catch (refreshError) {
+              console.log("엑세스 토큰 갱신 실패", refreshError);
+            }
+          } else {
+            console.log("에러발생", error);
           }
-        } else {
-          console.log("에러발생", error);
         }
       }
     };
 
     validateToken();
-  }, []); //cookies를 배열에 추가하면 리프레시 토큰이 변경되었을 때에 토큰 유효성 검사와 엑세스 토큰 갱신이 이루어짐.
+  }, [cookies.refreshToken]);
 
   return null;
 }
