2f9b85d83833f3cb3becc8c77c07bb231172e839
장정안 <wjddks96@naver.com>
2023-11-21 02:26:19 +0900

    feat:토큰 유효성 검사 메인페이지에서 호출

--- frontend/src/pages/Main/MainPage.jsx
+++ frontend/src/pages/Main/MainPage.jsx
@@ -2,8 +2,6 @@ import React, { useEffect } from "react";
 import TokenValidate from "../../apis/TokenValidate";
 
 export default function MainPage() {
-  useEffect(() => {
-    TokenValidate();
-  }, []);
+  <TokenValidate />;
   return <div>MainPage</div>;
 }
