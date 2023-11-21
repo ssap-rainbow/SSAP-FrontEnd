8ffebaeeb353964e059a3e9d87620634fc978a6d
장정안 <wjddks96@naver.com>
2023-11-21 21:06:35 +0900

    test: 홈페이지에 로그아웃 로직 테스트로 추가

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Home from "../../components/Home/Home";
+import { LogoutHandler } from "../../apis/Logout";
 
 function HomePage() {
+  const logout = LogoutHandler();
   return (
     <div>
       <Home />
+      <button onClick={logout}>로그아웃</button>
     </div>
   );
 }
