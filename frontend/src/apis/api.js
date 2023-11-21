078a2d1ba2afde397113feae3fe9580cd581aa85
장정안 <wjddks96@naver.com>
2023-11-21 17:01:43 +0900

    feat: axios interceptor 내에서 로그아웃 처리 실패로 인한 원상 복구

--- frontend/src/apis/api.js
+++ frontend/src/apis/api.js
@@ -1,40 +1,26 @@
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 // Axios 인스턴스 생성
 const api = axios.create({
-  baseURL: "https://k042b6e5a7030a.user-app.krampoline.com/",
+  baseURL: "https://your-api-url.com",
 });
 
-const ApiWrapper = () => {
-  const navigate = useNavigate();
+// Request 인터셉터 추가
+api.interceptors.request.use(
+  async (config) => {
+    // 엑세스 토큰을 세션 스토리지에서 가져오기
+    const accessToken = sessionStorage.getItem("accessToken");
 
-  const handleLogout = () => {
-    console.error("액세스 토큰이 없습니다. 로그아웃 처리를 진행합니다.");
-    sessionStorage.removeItem("accessToken");
-    navigate("/login");
-  };
-
-  // Request 인터셉터 추가
-  api.interceptors.request.use(
-    async (config) => {
-      const accessToken = sessionStorage.getItem("accessToken");
-
-      // 토큰이 있는 경우 헤더에 추가
-      if (accessToken) {
-        config.headers["Authorization"] = `Bearer ${accessToken}`;
-      } else {
-        handleLogout();
-      }
-
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
+    // 토큰이 있는 경우 헤더에 추가
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
     }
-  );
 
-  return null;
-};
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
