d14530db7a4e815df0c8ae0cf3544da6fce39fd9
장정안 <wjddks96@naver.com>
2023-11-21 16:50:19 +0900

    feat: 토큰이 없을 경우 로그아웃 처리 로직
    Logout.js에 로그아웃 로직이 담겨있음에도 불구하고, 토큰이 없을 때의 로그아웃 로직과는 좀 다른 측면이 있다고 판단되어,
    api.js에 간단한 로그아웃 로직을 추가하였습니다.

--- frontend/src/apis/api.js
+++ frontend/src/apis/api.js
@@ -1,25 +1,40 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 // Axios 인스턴스 생성
 const api = axios.create({
   baseURL: "https://k042b6e5a7030a.user-app.krampoline.com/",
 });
 
-// Request 인터셉터 추가
-api.interceptors.request.use(
-  async (config) => {
-    const accessToken = sessionStorage.getItem("accessToken");
+const ApiWrapper = () => {
+  const navigate = useNavigate();
 
-    // 토큰이 있는 경우 헤더에 추가
-    if (accessToken) {
-      config.headers["Authorization"] = `Bearer ${accessToken}`;
+  const handleLogout = () => {
+    console.error("액세스 토큰이 없습니다. 로그아웃 처리를 진행합니다.");
+    sessionStorage.removeItem("accessToken");
+    navigate("/login");
+  };
+
+  // Request 인터셉터 추가
+  api.interceptors.request.use(
+    async (config) => {
+      const accessToken = sessionStorage.getItem("accessToken");
+
+      // 토큰이 있는 경우 헤더에 추가
+      if (accessToken) {
+        config.headers["Authorization"] = `Bearer ${accessToken}`;
+      } else {
+        handleLogout();
+      }
+
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
     }
+  );
 
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+  return null;
+};
 
 export default api;
