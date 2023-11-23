00faa834420a6618dadd38f188c179731c82482d
Seongsuk Hong <ssukh77@naver.com>
2023-11-23 15:13:02 +0900

    fix(handleLogout 이벤트 핸들러 UserProfile 함수 내부로 이동) #10

--- frontend/src/components/@common/Header/Menu/UserProfile.tsx
+++ frontend/src/components/@common/Header/Menu/UserProfile.tsx
@@ -4,12 +4,12 @@ import defaultProfileImg from "../../../../assets/images/ssap_icon.svg";
 import { user } from "../../../../mocks/userData";
 import { LogoutHandler } from "../../../../apis/Logout";
 
-// 로그아웃 처리
-const handleLogout = () => {
-  LogoutHandler();
-};
-
 function UserProfile() {
+  // 로그아웃 처리
+  const handleLogout = () => {
+    LogoutHandler();
+  };
+
   return (
     <UserProfileWrapper>
       <UserImgBox>
