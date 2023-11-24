e46b7a463ab5b40eb8254e4bd933befe17a2f717
Seongsuk Hong <ssukh77@naver.com>
2023-11-24 23:50:06 +0900

    refactor(엑세스 토근 테스트용 코드 제거): #11

--- frontend/src/apis/category.ts
+++ frontend/src/apis/category.ts
@@ -1,12 +1,10 @@
-import { APP_URL, accessToken } from "./OAuth";
 import api from "./api";
 
 // 카테고리 리스트 가져오기
 export const getCategories = async () => {
   try {
-    const response = await api.get(`${APP_URL}/api/categories`, {
+    const response = await api.get(`/api/categories`, {
       headers: {
-        Authorization: `Bearer ${accessToken}`,
         // "Content-Type": "application/json",
       },
     });
@@ -21,9 +19,8 @@ export const getCategories = async () => {
 // 하위 카테고리 리스트 가져오기
 export const getSubCategories = async (categoryId) => {
   try {
-    const response = await api.get(`${APP_URL}/api/categories/${categoryId}/items`, {
+    const response = await api.get(`/api/categories/${categoryId}/items`, {
       headers: {
-        Authorization: `Bearer ${accessToken}`,
         // "Content-Type": "application/json",
       },
     });
